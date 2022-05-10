889ab6b06e3cc643d974aaaf6ddfabf0a8fbb1da
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-11 00:39:08 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -42,7 +42,7 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
     int tempStatus = isBatteryTemperatureOk(temperature);
     int socStatus = isBatteryChargeStateOk(soc);
     int chargeRateStatus = isBatteryChargeRateOk(chargeRate);
-     if(tempStatus == 0)
+     if(tempStatus == false || socStatus == false || chargeRateStatus == false)
      {
         return 0;
      }
