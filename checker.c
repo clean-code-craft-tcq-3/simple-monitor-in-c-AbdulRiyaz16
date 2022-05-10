c8fea82935fc6b1ce4ed1e6529d25202bf359b92
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-10 22:48:41 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -42,4 +42,10 @@ int main() {
   assert(isBatteryTemperatureOk(25));
   assert(!isBatteryTemperatureOk(50));
   assert(!isBatteryTemperatureOk(-1));
+  assert(isBatteryChargeStateOk(25));
+  assert(!isBatteryChargeStateOk(90));
+  assert(!isBatteryChargeStateOk(10));
+  assert(isBatteryChargeRateOk(1.0));
+  assert(!isBatteryChargeRateOk(0.8));
+  assert(!isBatteryChargeRateOk(0.7));
 }
