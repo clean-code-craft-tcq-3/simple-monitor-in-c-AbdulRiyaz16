8ede45438422079191dd0f4788cb2f2c584c09bd
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-11 00:31:03 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -39,7 +39,9 @@ int isBatteryTemperatureOk(float temperature)
 
 int batteryIsOk(float temperature, float soc, float chargeRate) 
 {
-  if ((isBatteryTemperatureOk(temperature) == 0))
+  if ((isBatteryTemperatureOk(temperature) == 0)
+     || (isBatteryChargeStateOk(soc) == 0) 
+     || (isBatteryChargeRateOk(chargeRate) == 0))
   {
     return 0;
   }
