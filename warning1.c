aa327d34d9f2cc33705f18db22920a2d49c93e53
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 21:02:41 +0530

    Update warning1.c

--- warning1.c
+++ warning1.c
@@ -1,14 +1,14 @@
 #include "batterymanagement.h"
 
-void printTemperaturewarning(int Temperaturewarning)
+void printTemperaturewarning1(int Temperaturewarning)
 {
   (Temperaturewarning==1) ? printf("Warning: Battery Temperature approaching lower limit\n") : ((Temperaturewarning==2)? printf("Warning: Battery Temperature approaching higher limit\n") : printf("No Early warning for Battery temperature\n"));
 }
-void printsocwarning(int socwarning)
+void printsocwarning1(int socwarning)
 {
   (socwarning==1) ? printf("Warning: Battery Approaching discharge\n") : ((socwarning==2)? printf("Warning: Warning: Battery Approaching charge-peak\n") : printf("No Early warning for Battery SOC\n"));
 }
-void printchargeratewarning(int chargeratewarning)
+void printchargeratewarning1(int chargeratewarning)
 {
    (chargeratewarning==1) ? printf("Warning: Battery Charge rate approaching lower limit\n") : ((chargeratewarning==2)? printf("Warning: Battery Charge rate approaching higher limit\n") : printf("No early warning for Battery charge rate\n"));
 }
