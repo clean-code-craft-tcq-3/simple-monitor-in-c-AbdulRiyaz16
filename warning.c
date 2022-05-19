7df162b46d59c99d2a76d992124c64643596d4fa
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 16:44:46 +0530

    Update warning.c

--- warning.c
+++ warning.c
@@ -22,3 +22,17 @@ void warning(float value,float minValue,float maxValue)
     return 2;
   }
  }
+
+int batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+   int tempStatus = isBatteryTemperatureOk(temperature);
+   int socStatus = isBatteryChargeStateOk(soc);
+   int chargeRateStatus = isBatteryChargeRateOk(chargeRate);
+   int invalid = tempStatus||socStatus||chargeRateStatus;
+   if(invalid==0)
+   {
+        return 0;
+   }
+  return 1 ;
+}
+
