1e1affd2ef64de235549b2875b883f6ce3bd0af8
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 17:13:46 +0530

    Update warning.c

--- warning.c
+++ warning.c
@@ -22,3 +22,28 @@ int warning(float value,float minValue,float maxValue)
   }
  }
 
+void printTemperaturewarning(int Temperaturewarning)
+{
+  if(languagePreffered==English)
+  printf("Battery Temperature ");
+  (Temperaturewarning==1) ? printf("Warning: Battery Temperature approaching lower limit\n") : ((Temperaturewarning==2)? printf("Warning: Battery Temperature approaching higher limit\n") : printf("No warning for temperature\n"));
+  else if(languagePreffered==German)
+  printf("Batterie Ladezustand");
+  (Temperaturewarning==1) ? printf("Warnung: Batterietemperatur nähert sich dem unteren Grenzwert\n") : ((Temperaturewarning==2)? printf("Warnung: Batterietemperatur nähert sich dem oberen Grenzwert\n") : printf("Keine Frühwarnung für Temperatur\n")); 
+}
+void printsocwarning(int)
+{
+  if(languagePreffered==English)
+  printf("Battery Temperature ");
+  else if(languagePreffered==German)
+  printf("Batterie Ladezustand");
+
+}
+void printTemperaturewarning(int)
+{
+   if(languagePreffered==English)
+  printf("Battery Temperature ");
+  else if(languagePreffered==German)
+  printf("Batterie Ladezustand");
+}
+
