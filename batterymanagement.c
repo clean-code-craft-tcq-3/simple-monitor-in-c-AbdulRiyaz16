7c200981dc2877553919332c298cd1599df4e1d6
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 13:52:34 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -1,44 +1,8 @@
 #include "batterymanagement.h"
 
-int isBatteryChargeRateOk(float chargeRate)
-{
-    if(chargeRate > MAX_CHARGERATE)
-    {
-        printf("Charge Rate out of range!\n");
-        return 0;
-    }
-    return 1;
-}
-
-int isBatteryChargeStateOk(float soc)
-{
-    if(soc < MIN_CHARGESTATE || soc > MAX_CHARGESTATE) 
-    {
-        printf("State of Charge out of range!\n");
-        return 0;
-    }
-    return 1;
-}
-
-
-int isBatteryTemperatureOk(float temperature)
-{
-  if(temperature < MIN_TEMPERATURE || temperature > MAX_TEMPERATURE)
-  {
-    printf("Temperature out of range!\n");
-    return 0;
-  }
-  return 1;
-}
 
 int main() {
-  assert(isBatteryTemperatureOk(25));
-  assert(!isBatteryTemperatureOk(50));
-  assert(!isBatteryTemperatureOk(-1));
-  assert(isBatteryChargeStateOk(25));
-  assert(!isBatteryChargeStateOk(90));
-  assert(!isBatteryChargeStateOk(10));
-  assert(!isBatteryChargeRateOk(1.0));
-  assert(!isBatteryChargeRateOk(0.8));
-  assert(isBatteryChargeRateOk(0.7));
+  assert(toleranceCheck(25,MIN_TEMPERATURE,MAX_TEMPERATURE));
+  assert(!toleranceCheck(50,MIN_TEMPERATURE,MAX_TEMPERATURE));
+  assert(!toleranceCheck(-1,MIN_TEMPERATURE,MAX_TEMPERATURE));
 }
