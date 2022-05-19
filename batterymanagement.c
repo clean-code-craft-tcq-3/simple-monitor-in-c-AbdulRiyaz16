33c0c82298e87d85639fd3c063cdaceb69d567c4
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 16:41:01 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -29,6 +29,19 @@ int isBatteryTemperatureOk(float temperature)
   return toleranceCheck(temperature,MIN_TEMPERATURE,MAX_TEMPERATURE);
 }
 
+int batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+   int tempStatus = isBatteryTemperatureOk(temperature);
+   int socStatus = isBatteryChargeStateOk(soc);
+   int chargeRateStatus = isBatteryChargeRateOk(chargeRate);
+   int invalid = tempStatus||socStatus||chargeRateStatus;
+   if(invalid==0)
+   {
+        return 0;
+   }
+  return 1 ;
+}
+
 
 int main() {
   languagePreffered=English;
