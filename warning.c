18d4e6f2d88917670abb7f106cca811bd8bfbddc
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 13:49:50 +0530

    Update warning.c

--- warning.c
+++ warning.c
@@ -4,3 +4,13 @@ float toleranceBaseValue(float maxlimit)
 {
     return ((WARNING_PERCENTAGE/MAX_PERCENTAGE)*maxlimit);
 }
+
+int toleranceCheck(float value,float minValue,float maxValue)
+{
+  int alertcount=0;
+  if (value < minValue|| value > maxValue) 
+  {
+     alertcount=1;
+  }
+  return alertcount;
+}
