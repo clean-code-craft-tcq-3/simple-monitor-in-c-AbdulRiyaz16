9441fc7fb84d45ad7627b04c2d0bfa496da638ab
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 21:02:14 +0530

    Update warning2.c

--- warning2.c
+++ warning2.c
@@ -1,14 +1,14 @@
   #include "batterymanagement.h"
 
-void printTemperaturewarningL2(int Temperaturewarning)
+void printTemperaturewarning2(int Temperaturewarning)
 {
   (Temperaturewarning==1) ? printf("Warnung: Batterietemperatur nähert sich dem unteren Grenzwert\n") : ((Temperaturewarning==2)? printf("Warnung: Batterietemperatur nähert sich dem oberen Grenzwert\n") : printf("Keine Frühwarnung für Temperature\n"));
 }
-void printsocwarningL2(int socwarning)
+void printsocwarning2(int socwarning)
 {
   (socwarning==1) ? printf("Warnung: Batterie fast entladen\n") : ((socwarning==2)? printf("Warnung: Batterie nähert sich der Ladespitze\n") : printf("Keine Frühwarnung für SOC\n"));
 }
-void printchargeratewarningL2(int chargeratewarning)
+void printchargeratewarning2(int chargeratewarning)
 {
   (chargeratewarning==1) ? printf("Warnung: Batterieladerate nähert sich dem unteren Grenzwert\n") : ((chargeratewarning==2)? printf("Warnung: Batterieladerate nähert sich dem oberen Limit\n") : printf("Keine Frühwarnung für den Gebührensatz\n"));
 }
