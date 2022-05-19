bc0879918ea087c93a0e8fee084184cfde9f8468
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 21:31:15 +0530

    Delete Batterystate.c

--- Batterystate.c
+++
@@ -1,9 +0,0 @@
-#include "batterymanagement.h"
-
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-   int tempStatus = isBatteryTemperatureOk(temperature);
-   int socStatus = isBatteryChargeStateOk(soc);
-   int chargeRateStatus = isBatteryChargeRateOk(chargeRate);
-   return 1 ;
-}
