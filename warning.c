36e8df949f7ef2283e20d43e9d94438496b8e493
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 13:30:35 +0530

    Update warning.c

--- warning.c
+++ warning.c
@@ -4,15 +4,3 @@ float toleranceBaseValue(float maxlimit)
 {
     return ((WARNING_PERCENTAGE/MAX_PERCENTAGE)*maxlimit);
 }
-
-void toleranceCheck(float CurrentValue, float toleranceBaseValue)
-{
-  if(CurrentValue >= MIN_TEMPERATURE && CurrentValue <= (MIN_TEMPERATURE+toleranceBaseValue)) 
-  {
-      printf("Warning: Approaching Temperature below thershold\n");
-  }
-  else if (CurrentValue >= MAX_TEMPERATURE && CurrentValue <= (MAX_TEMPERATURE-toleranceBaseValue))
-  {
-      printf("Warning: Approaching Temperature peak thershold\n");
-  } 
-}
