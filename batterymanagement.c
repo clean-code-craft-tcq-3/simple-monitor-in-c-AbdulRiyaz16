148eb5a0710c7534ff6de18a73b628db2ddce6e0
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 14:03:16 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -1,8 +1,13 @@
 #include "batterymanagement.h"
 
-
 int main() {
   assert(toleranceCheck(25,MIN_TEMPERATURE,MAX_TEMPERATURE));
   assert(!toleranceCheck(50,MIN_TEMPERATURE,MAX_TEMPERATURE));
   assert(!toleranceCheck(-1,MIN_TEMPERATURE,MAX_TEMPERATURE));
+  assert(toleranceCheck(25,MIN_CHARGESTATE,MAX_CHARGESTATE));
+  assert(!toleranceCheck(90,MIN_CHARGESTATE,MAX_CHARGESTATE));
+  assert(!toleranceCheck(10,MIN_CHARGESTATE,MAX_CHARGESTATE));
+  assert(!toleranceCheck(1.0,0,MAX_CHARGERATE));
+  assert(!toleranceCheck(0.8,0,MAX_CHARGERATE));
+  assert(toleranceCheck(0.7,0,MAX_CHARGERATE));
 }
