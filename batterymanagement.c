5c4a6a8aa11fe341520f233c1172d43727d69773
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 14:06:50 +0530

    Update batterymanagement.c

--- batterymanagement.c
+++ batterymanagement.c
@@ -8,6 +8,6 @@ int main() {
   assert(!toleranceCheck(90,MIN_CHARGESTATE,MAX_CHARGESTATE));
   assert(!toleranceCheck(10,MIN_CHARGESTATE,MAX_CHARGESTATE));
   assert(!toleranceCheck(1.0,0,MAX_CHARGERATE));
-  assert(!toleranceCheck(0.8,0,MAX_CHARGERATE));
+  assert(toleranceCheck(0.8,0,MAX_CHARGERATE));
   assert(toleranceCheck(0.7,0,MAX_CHARGERATE));
 }
