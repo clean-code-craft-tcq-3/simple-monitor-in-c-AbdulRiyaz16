92ce0d7e3614c516a5f006636431c1a249e6ccf3
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 13:10:19 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,5 +1,21 @@
 #include "batterymanagement.h"
 
+float toleranceBaseValue(float maxlimit)
+{
+    return ((WARNING_PERCENTAGE/MAX_PERCENTAGE)*maxlimit);
+}
+void toleranceCheck(float CurrentValue, float toleranceBaseValue)
+{
+  if(CurrentValue >= MIN_TEMPERATURE && CurrentValue <= MIN_TEMPERATURE+toleranceBaseValue) 
+  {
+      printf("Warning: Approaching Temperature below thershold\n");
+  }
+  else if (CurrentValue >= MAX_TEMPERATURE && CurrentValue <= MAX_TEMPERATURE-toleranceBaseValue)
+  {
+      printf("Warning: Approaching Temperature peak thershold\n");
+  } 
+}
+
 int isBatteryTemperatureOk(float temperature)
 {
   if(temperature < MIN_TEMPERATURE || temperature > MAX_TEMPERATURE)
