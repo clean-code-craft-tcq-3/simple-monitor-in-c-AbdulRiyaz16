319d67973543879077117d2bdb980d7ee74f6729
AbdulRiyaz16 <91129670+AbdulRiyaz16@users.noreply.github.com>
2022-05-19 21:19:25 +0530

    Update Batterystate.c

--- Batterystate.c
+++ Batterystate.c
@@ -5,10 +5,5 @@ int batteryIsOk(float temperature, float soc, float chargeRate)
    int tempStatus = isBatteryTemperatureOk(temperature);
    int socStatus = isBatteryChargeStateOk(soc);
    int chargeRateStatus = isBatteryChargeRateOk(chargeRate);
-   int invalid = tempStatus||socStatus||chargeRateStatus;
-   if(invalid==0)
-   {
-        return 0;
-   }
-  return 1 ;
+   return 1 ;
 }
